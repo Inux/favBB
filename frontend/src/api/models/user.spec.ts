fb6f7105e37fd690791357d9b468ffedea61f580
inux <inux.steve@gmail.com>
2020-02-08 02:35:10 +0100

    implemented users model and tests

--- frontend/src/api/models/user.spec.ts
+++ frontend/src/api/models/user.spec.ts
@@ -1,13 +1,38 @@
 import User from "./user";
 
 test('should return valid User', () => {
-    let user = new User(0, "test");
+    let user = new User(0, "name", "secret");
     expect(user.id).toBe(0);
-    expect(user.name).toBe("test");
+    expect(user.name).toBe("name");
+    expect(user.password).toBe("secret");
 });
 
+// fromJson Tests
+
 test('should return valid User from JSON', () => {
-    let user = User.fromJson('{"id":0,"name":"test"}');
+    let user = User.fromJson('{"id":0,"name":"name","password":"secret"}');
     expect(user.id).toBe(0);
-    expect(user.name).toBe("test");
+    expect(user.name).toBe("name");
+    expect(user.password).toBe("secret");
+});
+
+test('should throw expection because of missing id in JSON', () => {
+    const f = () => {
+        User.fromJson('{"name":"name","password":"secret"}');
+    };
+    expect(f).toThrow(TypeError("provided JSON does not contain all fields (id, name, password)."));
+});
+
+test('should throw expection because of missing name in JSON', () => {
+    const f = () => {
+        User.fromJson('{"id":0,"password":"secret"}');
+    };
+    expect(f).toThrow(TypeError("provided JSON does not contain all fields (id, name, password)."));
+});
+
+test('should throw expection because of missing password in JSON', () => {
+    const f = () => {
+        User.fromJson('{"name":"name","id":0}');
+    };
+    expect(f).toThrow(TypeError("provided JSON does not contain all fields (id, name, password)."));
 });
--- frontend/src/api/models/user.ts
+++ frontend/src/api/models/user.ts
@@ -1,18 +1,20 @@
 class User {
     id: number;
     name: string;
+    password: string;
 
-    constructor(id: number, name: string) {
+    constructor(id: number, name: string, password: string) {
         this.id = id;
         this.name = name;
+        this.password = password;
     }
 
     static fromJson(json: string): User {
         const obj = JSON.parse(json);
-        if(obj.id === undefined || obj.name === undefined) {
-            throw TypeError("provided JSON does not contain id and/or name.");
+        if(obj.id === undefined || obj.name === undefined || obj.password === undefined) {
+            throw TypeError("provided JSON does not contain all fields (id, name, password).");
         }
-        return new User(obj.id, obj.name);
+        return new User(obj.id, obj.name, obj.password);
     }
 }
 
